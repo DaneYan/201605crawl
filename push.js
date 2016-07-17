80c5d3b04c7b4f162b8c25218b8450d467d24d4d
zhufengnodejs <zhufengnodejs@126.com>
2016-07-17 15:16:46 +0800

    打印日志

--- push.js
+++ push.js
@@ -25,6 +25,7 @@ process.stdin.on('data',function(data){
     exec('git add -A',function(err,stdout,stderr){
         exec('git commit -m"'+data.toString()+'"',function(err,stdout,stderr){
             exec('git push origin master',function(err,stdout,stderr){
+                console.log(stdout);
                 console.log('提交完毕');
                 process.exit(0);
             });
