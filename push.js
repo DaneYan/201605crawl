b814b457fe7253bb108a539d5ad358cb8d288d3d
zhufengnodejs <zhufengnodejs@126.com>
2016-07-17 11:42:43 +0800

    合并命令

--- push.js
+++ push.js
@@ -20,12 +20,21 @@ job.start();*/
     });
 });*/
 
+/*
 process.stdin.on('data',function(data){
     exec('git add -A',function(err,stdout,stderr){
         exec('git commit -m"'+data.toString()+'"',function(err,stdout,stderr){
             exec('git push origin master',function(err,stdout,stderr){
                 console.log('提交完毕');
+                process.exit(0);
             });
         });
+    });
+});*/
+
+process.stdin.on('data',function(data){
+    exec('git add -A && git commit -m"'+data.toString()+'" && git push origin master',function(err,stdout,stderr){
+        console.log('提交完毕');
+        process.exit(0);
     });
 });
