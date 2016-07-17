9362af6baaff498219b236e2a01304fe05b8049a
zhufengnodejs <zhufengnodejs@126.com>
2016-07-17 11:02:06 +0800

    编码

---
+++ prepare/debug.js
@@ -0,0 +1,9 @@
+module.exports = function(name){
+   return function(msg){
+       var start = Date.now();
+       var debug = process.env.DEBUG;
+       if(new RegExp(debug).test(name)){
+           console.log(name,msg,(Date.now()-start)+'ms');
+       }
+   }
+}
